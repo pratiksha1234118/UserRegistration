5fa917b9d54d9b5d3881b897923d327316b60d96
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-11 16:56:49 +0530

    modified :comma is added in curly bracket

--- pattern.sh
+++ pattern.sh
@@ -1,6 +1,6 @@
 echo "Enter the Password:";
 read PASSWORD;
-pass="^[a-z]{8,}[A-Z]{1}[0-9]{1}[#$%&*^@]{1}$";
+pass="^[a-z]{8,}[A-Z]{1,}[0-9]{1,}[#$%&*^@]{1}$";
 if [[ $PASSWORD =~ $pass ]]
 then
 echo "Password Is Valid";
