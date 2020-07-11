07f2240e565160cd4831dc8ae05baed4dea7e1ed
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-11 14:10:32 +0530

    Added :First name is added

--- pattern.sh
+++ pattern.sh
@@ -1,1 +1,14 @@
 echo User Registration System needs to ensure all validations are in place during the User Entry
+#!/bin/bash -x
+#first name
+echo "Enter your first name"
+read FName;
+pattern="^[A-Z]{1}[a-z]{2,}$";
+if [[ $FName =~ $pattern ]]
+then
+        echo "First name is valid";
+else
+        echo "First name is invalid";
+fi
+
+
